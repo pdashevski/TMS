781031e1b6e79fe5159523a68c5aa54ad3d68dca
Pavel Dashevski <paveldashevski@gmail.com>
2020-12-02 00:35:57 +0300

    Modifying Task6

--- JavaHomeTasks/src/com/javaHome/task2/Task6.java
+++ JavaHomeTasks/src/com/javaHome/task2/Task6.java
@@ -1,9 +1,22 @@
 package com.javaHome.task2;
 
+/*
+    6. И ещё можете попрактиковаться и нарисовать оставшиеся 2
+    треугольника
+    *
+    * * (1)
+    * * *
+    * * * *
+
+    * * * *
+    * * * (2)
+    * *
+    *
+ */
+
 public class Task6 {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++) {
-            //System.out.println("*");
+        for(int i = 0; i < 4; i++) {
             for(int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
@@ -13,10 +26,10 @@ public class Task6 {
         System.out.println("");
 
         for(int i = 4; i != 0; i--) {
-            System.out.println("*");
             for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
