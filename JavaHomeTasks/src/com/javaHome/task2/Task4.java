092c2ec7c4e70cb08468eacbaebeb138639d0749
Pavel Dashevski <paveldashevski@gmail.com>
2020-12-02 00:39:50 +0300

    Modifying Task4

--- JavaHomeTasks/src/com/javaHome/task2/Task4.java
+++ JavaHomeTasks/src/com/javaHome/task2/Task4.java
@@ -1,5 +1,10 @@
 package com.javaHome.task2;
 
+/*
+    4. Напишите программу печати таблицы перевода расстояний из дюймов в
+    сантиметры для значений длин от 1 до 20 дюймов. 1 дюйм = 2,54 см
+ */
+
 public class Task4 {
     public static void main(String[] args) {
         double oneInch = 2.54;
