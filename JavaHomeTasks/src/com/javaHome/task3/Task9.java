8bf7f9409d1eb38963041d456831722a770be747
Pavel Dashevski <paveldashevski@gmail.com>
2020-12-05 00:10:15 +0300

    task9 update

---
+++ JavaHomeTasks/src/com/javaHome/task3/Task9.java
@@ -0,0 +1,34 @@
+package com.javaHome.task3;
+
+/*
+    9. Пользователь должен указать с клавиатуры положительное число, а
+    программа должна создать массив указанного размера из случайных целых
+    чисел из [0;15] и вывести его на экран в строку. После этого программа должна
+    определить и сообщить пользователю о том, сумма какой половины массива
+    больше: левой или правой, либо сообщить, что эти суммы модулей равны. Если
+    пользователь введёт неподходящее число, то выдать соответствующее
+    сообщение
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Task9 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Random randomNumber = new Random();
+        System.out.println("Введите положительное число:");
+        String s = reader.readLine();
+        int arraySize = Integer.parseInt(s);
+        int[] array = new int[arraySize];
+
+        for (int i = 0; i < array.length; i ++) {
+            array[i] = randomNumber.nextInt(15);
+            System.out.print(array[i] + " ");
+        }
+
+
+    }
+}
