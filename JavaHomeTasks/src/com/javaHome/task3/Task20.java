ec431f66440c7057683dbb577991340c43190e49
Pavel Dashevski <paveldashevski@gmail.com>
2020-12-06 16:43:56 +0300

    task20 update

---
+++ JavaHomeTasks/src/com/javaHome/task3/Task20.java
@@ -0,0 +1,46 @@
+package com.javaHome.task3;
+
+/*
+    10) Найти максимальный элемент в массиве и поменять его местами с нулевым
+    элементом
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task20 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите длину одномерного массива:");
+        String s = reader.readLine();
+        int arraySize = Integer.parseInt(s);
+        int[] array = new int[arraySize];
+        Random randomNumber = new Random();
+        int min = 0,max = 0;
+        int temp;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = randomNumber.nextInt(10);
+            if (array[min] > array[i]) {
+                min = i;
+            }
+            if (array[max] < array[i]) {
+                max = i;
+            }
+        }
+
+        System.out.print("\n");
+        System.out.println(Arrays.toString(array));
+        System.out.println("Min: " + array[min]);
+        System.out.println("Max: " + array[max]);
+
+        temp = array[min];
+        array[min] = array[max];
+        array[max] = temp;
+
+        System.out.println(Arrays.toString(array));
+    }
+}
