4d487af3fb905a7c0a692fe2d7f95975bdd10405
Pavel Dashevski <paveldashevski@gmail.com>
2020-12-23 01:06:54 +0300

    update task8: 3

---
+++ JavaHomeTasks/src/com/javaHome/task8/Strings3.java
@@ -0,0 +1,21 @@
+package com.javaHome.task8;
+
+/*
+    3. Перевернуть строку, изменив расположение символов в строке задом наперёд.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Strings3 {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Input string:");
+            StringBuilder sb = new StringBuilder(reader.readLine());
+            System.out.println(sb.reverse());
+        }
+        catch (Exception e) {
+
+        }
+    }
+}
