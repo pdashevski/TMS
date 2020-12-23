504c32d1c53a033c8fd98152c43f0a481a0aca7c
Pavel Dashevski <paveldashevski@gmail.com>
2020-12-23 23:47:00 +0300

    update task8: 6

--- JavaHomeTasks/src/com/javaHome/task8/Strings6.java
+++ JavaHomeTasks/src/com/javaHome/task8/Strings6.java
@@ -14,34 +14,20 @@ package com.javaHome.task8;
 public class Strings6 {
     public static void main(String[] args) {
         String s = "Object-oriented programming is a programming language model organized around" +
-                "objects rather than \"actions\" and data rather than logic. Object-oriented programming" +
+                " objects rather than \"actions\" and data rather than logic. Object-oriented programming" +
                 " blabla. Object-oriented programming bla.";
-
         System.out.println(s);
-
-        System.out.println("----------");
-
-
-
-
-
-        String[] lines = s.split(" ");
-
-
-        for (String line : lines) {
-            //находим индекс первого вхождения символа ":" в подстроке
-            int pos = line.indexOf("-");
-            //вычленяем имя атрибута из подстроки
-            String attributeName = line.substring(0, pos);
-            //вычленяем значение атрибута
-            String value = line.substring(pos + 1, line.length());
-            //вывод на экран вычлененных значений в нужном нам формате.
-            System.out.println(attributeName + " - " + value);
-        }
-
-
+        change(s);
     }
 
-
-
+    public static void change(String s) {
+        System.out.println("----------");
+        String[] subStr;
+        String delimiter = "\\."; // Разделитель
+        subStr = s.split(delimiter);
+        subStr[1] = "OOP blabla";
+        for (String ss : subStr) {
+            System.out.print(ss + ". ");
+        }
+    }
 }
